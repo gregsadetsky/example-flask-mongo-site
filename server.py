5fb4ba119f73e905b0568c2826e71aadb2c71e03
gregsadetsky <lepetitg@gmail.com>
2025-05-11 16:18:50 -0400

    .

--- server.py
+++ server.py
@@ -22,7 +22,7 @@ def hello_world():
         "author": "Mike",
         "text": "My first blog post!",
         "tags": ["mongodb", "python", "pymongo"],
-        "date": datetime.datetime.now(tz=datetime.timezone.utc),
+        "date": datetime.now(tz=datetime.timezone.utc),
     }
     post_id = posts.insert_one(post).inserted_id
 
