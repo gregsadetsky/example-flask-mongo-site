f216498f9ecdc1d1917d62e3ff03543f5a5cddb5
gregsadetsky <lepetitg@gmail.com>
2025-05-11 16:16:04 -0400

    .

---
+++ server.py
@@ -0,0 +1,34 @@
+from datetime import datetime
+
+import pymongo
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello_world():
+    print("new web request")
+
+    client = pymongo.MongoClient("mongo-mongo", 27017)
+    print("client", client)
+    db = client.test
+    print("db", db)
+    print("db.name", db.name)
+    print("db.collection_names()", db.collection_names())
+    posts = db.posts
+    print("posts", posts)
+
+    post = {
+        "author": "Mike",
+        "text": "My first blog post!",
+        "tags": ["mongodb", "python", "pymongo"],
+        "date": datetime.datetime.now(tz=datetime.timezone.utc),
+    }
+    post_id = posts.insert_one(post).inserted_id
+
+    return f"hello from disco!!! inserted post with id {post_id}!"
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080, debug=True)
