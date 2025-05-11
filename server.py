8830382cc3cc56f95d239efbae76f373476e34b0
gregsadetsky <lepetitg@gmail.com>
2025-05-11 16:25:15 -0400

    .

--- server.py
+++ server.py
@@ -5,28 +5,28 @@ from flask import Flask
 
 app = Flask(__name__)
 
+# assumes that the
+# https://github.com/letsdiscodev/example-mongo
+# repo has been deployed as a separate Disco project
+client = pymongo.MongoClient("mongo-mongo", 27017)
+
 
 @app.route("/")
 def hello_world():
     print("new web request")
 
-    client = pymongo.MongoClient("mongo-mongo", 27017)
-    print("client", client)
     db = client.test
-    print("db", db)
-    print("db.name", db.name)
     posts = db.posts
-    print("posts", posts)
 
     post = {
         "author": "Mike",
-        "text": "My first blog post!",
+        "text": "A new blog post!",
         "tags": ["mongodb", "python", "pymongo"],
         "date": datetime.now(),
     }
     post_id = posts.insert_one(post).inserted_id
 
-    return f"hello from disco!!! inserted post with id {post_id}!"
+    return f"hello from disco!!! inserted post into Mongo with id {post_id}!"
 
 
 if __name__ == "__main__":
